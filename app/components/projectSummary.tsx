f842cdbc438bdeb4490b319f5948213509c545e6
James <mail@jamesaproctor.com>
2026-07-26 16:54:15 -0700

    tweak spacing

--- app/components/projectSummary.tsx
+++ app/components/projectSummary.tsx
@@ -5,7 +5,7 @@ import { Metadata } from '@/app/types';
 export default function ProjectSummary({ metadata }: { metadata: Metadata }) {
   const { title, description, category, client, industry, link } = metadata;
   return (
-    <div className='flex flex-col gap-2 mb-2'>
+    <div className='flex flex-col gap-2 my-2'>
       <h3>{title}</h3>
       <div className='flex text-sm gap-2'>
         {[client, industry, category].map((v, i) => (
