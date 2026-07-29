eaeb4501caecff415ccf66aee174c0fc5f6159cb
James <mail@jamesaproctor.com>
2026-07-28 21:07:17 -0700

    setup for docker deploy to lab

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['smith.local']
+  allowedDevOrigins: ['smith.local'],
+  output: "standalone",
 };
 
 export default nextConfig;
